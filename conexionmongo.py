94e0a6455844a3250f6341ebbfcece61b18b3f50
MiguelMonr <miguemonrealjr@gmail.com>
2023-11-29 23:36:23 -0600

    Conexiones

--- conexionmongo.py
+++ conexionmongo.py
@@ -7,7 +7,7 @@ api_key = 'c8143bf74c1d797a6756f96808315a09'
 
 
 #Conexión a la base de datos
-client = pymongo.MongoClient("mongodb://mongo_lake:27017")
+client = pymongo.MongoClient("mongodb://localhost:27017")
 
 mongo = client["IMDB_m"]
 
@@ -17,9 +17,8 @@ movie_data_list = []
 # Lista de claves IMDb (deberás reemplazar esto con la lista obtenida de tu archivo CSV)
 keys = list(pd.read_csv("imdb_keys.csv")["keys"])
 
-client = pymongo.MongoClient("mongodb://mongo_lake:27017")
-db = client["cocktails"]
-casa = db["drinks"]
+client = pymongo.MongoClient("mongodb://localhost:27017")
+db = client["movies"]
 mongo = db["IMDB_m"]
 
 # Bucle para recorrer cada clave y realizar la solicitud a la API
@@ -34,10 +33,11 @@ for i in range(10):
     if response.status_code == 200:
         # Añadir los datos de la película a la lista
         datos_movie= response.json()
+        db.IMDB_m.insert_one(datos_movie)
         print(f"Guardado movie #{i}: {datos_movie['movie_results']}")
     else:
         print(f"Failed to retrieve data for {key}. Status code:", response.status_code)
             
+
 client.close()
 # Ahora movie_data_list contiene los datos de todas las películas solicitadas
-#print(movie_data_list[0])
